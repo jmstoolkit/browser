c0108b71e7a9631a78dfdf40531e7f59b8ae6384
Scott Douglass <scott@swdouglass.com>
2017-11-08 22:40:09 -0500

    cleaning

---
+++ bin/browser.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BIN_DIR=$(dirname $0)
+. $BIN_DIR/jtk.sh
+COMMAND=com.jmstoolkit.queuebrowser.QueueBrowserApp
+JAVA_OPTS="-Djava.util.logging.config.file=logging.properties"
+# Change the name of the properties file:
+#JAVA_OPTS="-Dapp.properties=myfile.props -Djndi.properties=some.props"
+
+java $JAVA_OPTS $COMMAND $*
+
